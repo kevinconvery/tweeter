7b849d3365b5f5b2ce71cc22fdd6fd2728fb5c2b
Kevin Convery <kevinconvery@gmail.com>
2019-09-11 00:33:04 -0400

    dynamically getting tweets from /tweets route

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,47 +1,3 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
-
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
 const createTweetElement = function(tweetElement) {
   // Returning a large template string is a lot cleaner.
   return `<article class="tweet">
@@ -77,4 +33,33 @@ const renderTweets = function(tweets) {
   }
 }
 
-renderTweets(data);
+$(document).ready(function() {
+  // create-tweet submit event listener
+  // adds a new tweet to the tweets.
+  $('#create-tweet').submit(function(event) {
+    // prevent default handling (in this case, the refresh of the page)
+    event.preventDefault();
+    if ($(this) === undefined) {
+      alert('error');
+    } 
+    $.ajax({
+      url: '/tweets',
+      method: 'POST',
+      data: $(this).serialize()
+    }).then(function() {
+      console.log(response);
+    });
+  });
+
+  const loadTweets = function() {
+    const response = $.ajax({
+      url: '/tweets',
+      method: 'GET'
+    })
+    response.done(function(tweets) {
+      renderTweets(tweets);
+    });
+  }
+
+  loadTweets();
+});
