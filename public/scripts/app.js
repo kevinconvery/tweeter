9a87c4c917e5718c52039f4e46943570abdf0173
Kevin Convery <kevinconvery@gmail.com>
2019-09-12 02:20:47 +0000

    added calculateDays function to render dates accurately.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,8 +1,15 @@
-const tweetAgeInDays = date => {
-
+// Calculating just 
+const calculateDays = function(date) {
+  const timeDifference = Date.now() - date;
+  const formattedDate = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  if (formattedDate === 0) {
+    return `Today`;
+  } else {
+    return `${formattedDate} days ago`;
+  }
 }
 
-const createTweetElement = tweetElement => {
+const createTweetElement = function(tweetElement) {
   // Returning a large template string is a lot cleaner.
   return `<article class="tweet">
             <div class="top-row">
@@ -19,7 +26,7 @@ const createTweetElement = tweetElement => {
             </div>
             <div class="bottom-row">
               <div class="bottom-left">
-                <span class="tweet-age">${new Date(parseInt(tweetElement.created_at))}</span>
+                <span class="tweet-age">${calculateDays(tweetElement.created_at)}</span>
               </div>
               <div class="bottom-right">
                 <i class="fa fa-flag"></i>
@@ -30,11 +37,11 @@ const createTweetElement = tweetElement => {
           </article>`;
 }
 
-const appendTweet = tweet => {
+const appendTweet = function(tweet) {
   $('#tweets-container').append(createTweetElement(tweet));
 }
 
-const renderTweets = tweets => {
+const renderTweets = function(tweets) {
   for (tweet of tweets) {
     // append returned HTML string:
     appendTweet(tweet);
@@ -44,7 +51,7 @@ const renderTweets = tweets => {
 $(document).ready(() => {
   // create-tweet submit event listener
   // adds a new tweet to the tweets.
-  $('#create-tweet').submit((event) => {
+  $('#create-tweet').submit(function(event) {
     event.preventDefault();
     // prevent default handling (in this case, the refresh of the page)
     if ($('#tweet-text').val() === "") {
@@ -56,7 +63,7 @@ $(document).ready(() => {
           <button class="error-kill-button">Kill Me</button>
          </div>`;
       $('.container').prepend(errorHTML);
-      $('.error-kill-button').click((event) => {
+      $('.error-kill-button').click(function(event) {
         $('div').remove('.error-message');
       });
     } else if ($('#tweet-text').val().length > 140) {
@@ -66,7 +73,7 @@ $(document).ready(() => {
           <p>Tweets are limited to a length of 140 characters.</p>
          </div>`;
       $('.container').prepend(errorHTML);
-      $('.error-kill-button').click((event) => {
+      $('.error-kill-button').click(function(event) {
         $('div').remove('.error-message');
       });
     } else {
@@ -74,7 +81,7 @@ $(document).ready(() => {
         url: '/tweets',
         method: 'POST',
         data: $(this).serialize()
-      }).then((response) => {
+      }).then(function(response) {
         $('#tweet-text').val('');
         $('.counter').text('140');
         appendTweet(response);
